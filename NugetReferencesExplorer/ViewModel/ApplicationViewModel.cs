f751fe141c452bf4293da251e31a47c53efbba73
Alberic <alberic.entropics@gmail.com>
2017-05-12 09:01:55 +0200

    UI Async loading (in progress)

--- NugetReferencesExplorer/ViewModel/ApplicationViewModel.cs
+++ NugetReferencesExplorer/ViewModel/ApplicationViewModel.cs
@@ -1,26 +1,53 @@
-﻿using NugetReferencesExplorer.Model.Domain;
+﻿using GalaSoft.MvvmLight;
+using NugetReferencesExplorer.Model.Domain;
 using NugetReferencesExplorer.Model.Repository;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NugetReferencesExplorer.ViewModel
 {
-    public class ApplicationViewModel
+    public class ApplicationViewModel : ViewModelBase
     {
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    this.RaisePropertyChanged(nameof(IsBusy));
+                }
+            }
+        }
+
+        private async void LoadPackageItemsAsync()
+        {
+            IsBusy = true;
+            this.PackageItems = await Task.Run(() => LocalPackageRepositoryFactory.Create().GetPackages(Properties.Settings.Default.sourcePath).Where(p => p.HasDifferentVersion).ToList());//.ForEach(x => x.LoadPackageInfos()));
+            IsBusy = false;            
+        }
 
         private IEnumerable<Package> _packageItems;
         public IEnumerable<Package> PackageItems
         {
             get
-            {
+            {                
                 if (_packageItems == null)
-                {                    
-                    _packageItems = LocalPackageRepositoryFactory.Create().GetPackages(Properties.Settings.Default.sourcePath).Where(p => p.HasDifferentVersion);
-                }
-                return _packageItems;
+                {
+                    this.LoadPackageItemsAsync();                    
+                }   
+                return _packageItems;              
+            }
+            set
+            {
+                _packageItems = value;
+                this.RaisePropertyChanged(nameof(PackageItems));
             }
         }
 
